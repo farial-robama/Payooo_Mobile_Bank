ed89f230ec43183dfa52b2930bc06621f919507f
farial-robama <farialrobama15@gmail.com>
2025-08-26 15:08:18 +0200

    use reusable functions

--- home.js
+++ home.js
@@ -1,21 +1,47 @@
 const validPin = 1234;
 
-// add money feature
+// function to get input values
+function getInputValueNumber(id) {
+    const inputField = document.getElementById(id)
+    const inputFieldValue = inputField.value
+    const inputFieldValueNumber = parseInt(inputFieldValue)
+    return inputFieldValueNumber
+}
+function getInputValue (id){
+    const inputField = document.getElementById(id)
+    const inputFieldValue = inputField.value
+    return inputFieldValue
+}
+// function to get innertext
+function getInnerText(id){
+    const element = document.getElementById(id)
+    const elementValue = element.innerText
+    const elementValueNumber = parseInt(elementValue)
 
-document
-  .getElementById("add-money-btn")
-  .addEventListener("click", function (e) {
+    return elementValueNumber
+}
+// function to set innerText
+function setInnerText(value){
+    console.log(value)
+    const availableBalanceElement = document.getElementById("available-balance")
+    availableBalanceElement.innerText = value
+}
+
+// add-money features
+document.getElementById("add-money-btn").addEventListener("click", function (e){
     e.preventDefault();
-    const bank = document.getElementById("bank").value;
+    const bank = getInputValue("bank");
     const accountNumber = document.getElementById("account-number").value;
 
-    const amount = parseInt(document.getElementById("add-amount").value);
+    const amount = getInputValueNumber("add-amount");
 
-    const pinNumber = parseInt(document.getElementById("add-pin").value);
+    if(amount<=0){
+        alert("invalid amount")
+        return;
+    }
 
-    const availableBalance = parseInt(
-      document.getElementById("available-balance").innerText
-    );
+    const pinNumber = getInputValueNumber("add-pin");
+    const availableBalance = getInnerText("available-balance");
 
     if (accountNumber.length < 11) {
       alert("Invalid account Number");
@@ -28,34 +54,89 @@ document
     }
 
     const totalNewAvailableBalance = amount + availableBalance;
-    document.getElementById("available-balance").innerText =
-      totalNewAvailableBalance;
-  });
+
+    setInnerText(totalNewAvailableBalance);
+   
+
+});
+
+// add money feature
+
+// document
+//   .getElementById("add-money-btn")
+//   .addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const bank = document.getElementById("bank").value;
+//     const accountNumber = document.getElementById("account-number").value;
+
+//     const amount = parseInt(document.getElementById("add-amount").value);
+
+//     const pinNumber = parseInt(document.getElementById("add-pin").value);
+
+//     const availableBalance = parseInt(
+//       document.getElementById("available-balance").innerText
+//     );
+
+//     if (accountNumber.length < 11) {
+//       alert("Invalid account Number");
+//       return;
+//     }
+
+//     if (pinNumber !== validPin) {
+//       alert("Invalid pin Number");
+//       return;
+//     }
+
+//     const totalNewAvailableBalance = amount + availableBalance;
+//     document.getElementById("available-balance").innerText =
+//       totalNewAvailableBalance;
+//   });
 
 //   cashout money feature
 
 document.getElementById("withdraw-btn").addEventListener("click", function (e) {
   e.preventDefault();
 
-  const amount = parseInt(document.getElementById("withdraw-amount").value);
+  const amount = getInputValueNumber("withdraw-amount");
 
-  const availableBalance = parseInt(
-    document.getElementById("available-balance").innerText
-  );
+  const availableBalance = getInnerText("available-balance");
 
+  if(amount<=0 || amount>availableBalance){
+        alert("invalid amount")
+        return
+    }
   const totalNewAvailableBalance = availableBalance - amount;
 
   console.log(totalNewAvailableBalance);
 
-  document.getElementById("available-balance").innerText =
-    totalNewAvailableBalance;
+  setInnerText(totalNewAvailableBalance);
 
-  if (amount > availableBalance) {
-  alert("Insufficient balance");
-  return;
-}
+  });
 
-});
+//   cashout money feature
+
+// document.getElementById("withdraw-btn").addEventListener("click", function (e) {
+//   e.preventDefault();
+
+//   const amount = parseInt(document.getElementById("withdraw-amount").value);
+
+//   const availableBalance = parseInt(
+//     document.getElementById("available-balance").innerText
+//   );
+
+//   const totalNewAvailableBalance = availableBalance - amount;
+
+//   console.log(totalNewAvailableBalance);
+
+//   document.getElementById("available-balance").innerText =
+//     totalNewAvailableBalance;
+
+//   if (amount > availableBalance) {
+//   alert("Insufficient balance");
+//   return;
+// }
+
+// });
 
   // toggling feature
 
